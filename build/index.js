369525bcbbe58f4e4b050596826f5c40b9f76222
sharkeron <sharkeron@gmail.com>
2017-06-28 00:56:27 +0300

    pug add

--- build/index.js
+++ build/index.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -68,27 +68,29 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__menu__ = __webpack_require__(1);
-
-var menu = __WEBPACK_IMPORTED_MODULE_0__menu__["a" /* default */](['Главная', 'Обо мне', 'Портфолио'], 'menu');
-document.body.appendChild(menu);
+/* harmony default export */ __webpack_exports__["a"] = (function (array, className) {
+    var menu = document.createElement("ul");
+    menu.className = className;
+    var listItems = '';
+    array.forEach(function(item) {
+        listItems += '<li>' + item + '</li>';
+    });
+    menu.innerHTML = listItems;
+    return menu;
+});
 
 /***/ }),
 /* 1 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__[] = (function (array, className) {
-	var menu = document.createElement('ul');
-	menu.className = className;
-	var listItems = '';
-	array.forEach(function(item) {
-		listItems += '<li>' + item + '<li>';
-	});
-	menu.innerHTML = listItems;
-	return menu;
-});
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_menu_menu__ = __webpack_require__(0);
+
+var menu = __WEBPACK_IMPORTED_MODULE_0__components_menu_menu__["a" /* default */](['Главная','Блог'], 'menu');
+document.body.appendChild(menu);
+
+console.log('in index.js')
 
 /***/ })
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-	source: path.join(__dirname, 'source'),
-	build: path.join(__dirname, 'build')
+    source: path.join(__dirname, 'source'),
+    build: path.join(__dirname, 'build')
 };
 
 module.exports = {
-	entry: PATHS.source + '/index.js',
-	output: {
-		path: PATHS.build,
-		filename: '[name].js'
-	},
-	plugins: [
-	new HtmlWebpackPlugin({
-		template: PATHS.source + '/index.pug'
-	})
-	],
-	module: {
-		rules: [
-		{
-			test: /\.pug$/,
-			loader: 'pug-loader',
-			options: {
-				pretty: true
-			}
-		}]
-	}
-}
+    entry: {
+        'index': PATHS.source + '/pages/index/index.js',
+        'blog': PATHS.source + '/pages/blog/blog.js'
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['index'],
+            template: PATHS.source + '/pages/index/index.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: PATHS.source + '/pages/blog/blog.pug'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
+};
